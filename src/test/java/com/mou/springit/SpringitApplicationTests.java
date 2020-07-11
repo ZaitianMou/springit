4f6fe19933025a7aa356e0ced0bbcfc8399d270b
Zaitian Mou <zaitian.mou@gmail.com>
2020-07-11 10:32:23 -0400

    Initial Commit for Spring Boot Reddit Clone.

---
+++ src/test/java/com/mou/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mou.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
